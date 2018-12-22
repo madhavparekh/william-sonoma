437f8395898fad4cfdc7cccc94e7c45b602831e4
Madhav Parekh <madhavparekh@gmail.com>
2018-12-21 16:38:26 -0800

    fixed cors

--- src/actions/content.js
+++ src/actions/content.js
@@ -2,8 +2,7 @@ import request from 'superagent';
 import ContentTypes from '../action-types/Content';
 const types = ContentTypes;
 
-const LISTING_URL =
-  'https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json';
+const LISTING_URL = '/services/catalog/v4/category/shop/new/all-new/index.json';
 
 const fetchListingsObj = (listings) => {
   return {
@@ -15,7 +14,6 @@ const fetchListingsObj = (listings) => {
 export function fetchListings() {
   return async (dispatch) => {
     const listings = await request.get(LISTING_URL);
-    console.log(listings.body);
     dispatch(fetchListingsObj(listings.body));
   };
 }
--- src/components/display/RenderListing.js
+++ src/components/display/RenderListing.js
@@ -5,14 +5,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import red from '@material-ui/core/colors/red';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -70,12 +64,10 @@ class RenderListing extends React.Component {
   }
 
   handleClickOpen() {
-    console.log('opening');
     this.setState({ open: true });
   }
 
   handleClose() {
-    console.log('closing');
     this.setState({ open: false });
   }
 
@@ -105,17 +97,6 @@ class RenderListing extends React.Component {
             title={name}
             onClick={() => this.handleClickOpen()}
           />
-          <CardContent>
-            <Typography component="p">{name}</Typography>
-          </CardContent>
-          <CardActions className={classes.actions} disableActionSpacing>
-            <IconButton aria-label="Add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="Share">
-              <ShareIcon />
-            </IconButton>
-          </CardActions>
         </Card>
         <Dialog
           open={this.state.open}
